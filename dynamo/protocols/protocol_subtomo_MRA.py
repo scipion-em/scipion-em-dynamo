ad9d9b6d984f7993b4b924da145ac9d6eb60d078
estrellafg <estrellafergi@gmail.com>
2019-12-19 16:30:53 +0100

    numerical params added to formulary (not all)

--- dynamo/protocols/protocol_subtomo_MRA.py
+++ dynamo/protocols/protocol_subtomo_MRA.py
@@ -27,7 +27,7 @@
 
 from os.path import join, basename
 from os import environ
-from pyworkflow.protocol.params import PointerParam, BooleanParam, IntParam
+from pyworkflow.protocol.params import PointerParam, BooleanParam, IntParam, StringParam, FloatParam, LEVEL_ADVANCED
 from pyworkflow.utils.path import makePath
 
 from dynamo.convert import writeSetOfVolumes, writeDynTable, readDynTable
@@ -50,50 +50,65 @@ class DynamoSubTomoMRA(ProtTomoSubtomogramAveraging):
                       pointerClass="SetOfSubTomograms",
                       label='Set of volumes',
                       help="Set of subtomograms to align with dynamo")
+        form.addParam('numberOfRounds', IntParam, label='Rounds', default=3, help="Number of rounds (from 1 to 8)")
+        form.addParam('numberOfIters', IntParam, label='Iterations', default=1, help="Number of iterations per round")
+        form.addParam('numberOfRefs', IntParam, label='References', default=1, help="Number of references")
 
         form.addSection(label='Templates')
         form.addParam('generateTemplate', BooleanParam, default=True,
                       label='Generate a template reference:', help="Generate a reference template based on parameters")
-
-        # Pointer to a reference
         form.addParam('templateRef', PointerParam, label="Template",
                       condition="not generateTemplate", pointerClass='Volume',
                       help='Template to be used')
-
-        # Generation template options
         form.addParam('useChosenSoP', BooleanParam, label='Use random chosen set of particles', default=False,
                       condition="generateTemplate", help="Use a random set of particles")
-
         form.addParam('numberOfParticles', IntParam, label='Number of references', default=50,
                       condition="generateTemplate and useChosenSoP", help="Number of references to generate automatically")
-
         form.addParam('useRandomTable', BooleanParam, label='Use a random table (rotate particles randomly)', default=False,
                       condition="generateTemplate", help="")
-
         form.addParam('compensateMissingWedge', BooleanParam, label='Compensate for missing wedge', default=False,
                       condition="generateTemplate", help="")
 
-        # Masks
         form.addSection(label='Masks')
-
         form.addParam('alignmentMask', PointerParam, label="Alignment mask",
                       pointerClass='VolumeMask',
                       help='Mask for the alignment')
-
         form.addParam('classificationMask', PointerParam, label="Classification mask",
                       pointerClass='VolumeMask', allowsNull=True,
                       help='Mask for the classification steps')
-
         form.addParam('fourierMask', PointerParam, label="Fourier mask on template",
                       pointerClass='VolumeMask', allowsNull=True,
                       help='A binary mask describing which fourier components of'
                            ' the initial average are known.')
-
         form.addParam('fscMask', PointerParam, label="FSC mask",
-                      pointerClass='VolumeMask', allowsNull=True,
-                      help='A direct space mask that will be imposed onto any couple of volumes when computing their FSC.')
+                      pointerClass='VolumeMask', allowsNull=True, help='A direct space mask that will be imposed onto '
+                                                                       'any couple of volumes when computing their FSC.')
 
-        # Add all numerical (and computational?) parameters of dynamo
+        form.addSection(label='Angular scanning')
+        form.addParam('coneAperture', IntParam, label='Cone aperture', default=360, help=" ")
+        form.addParam('coneSampling', IntParam, label='Cone sampling', default=60, help=" ")
+        form.addParam('coneFlip', IntParam, label='Cone flip', default=0, expertLevel=LEVEL_ADVANCED, help=" ")
+        form.addParam('freezeRef', IntParam, label='Freeze reference', default=0, expertLevel=LEVEL_ADVANCED, help=" ")
+        form.addParam('azymuthRange', IntParam, label='Azimuth rotation range', default=0, help=" ")
+        form.addParam('azymuthSampling', IntParam, label='Azimuth rotation sampling', default=1, help=" ")
+        form.addParam('azymuthFlip', IntParam, label='Azymuth flip', default=0, expertLevel=LEVEL_ADVANCED, help=" ")
+        form.addParam('azymuthFreeze', IntParam, label='Freeze azymuth reference', default=0, expertLevel=LEVEL_ADVANCED, help=" ")
+        form.addParam('refine', IntParam, label='Refine', default=6, help=" ")
+        form.addParam('refineFactor', IntParam, label='Refine factor', default=2, help=" ")
+
+        form.addSection(label='Shift/Threshold')
+        form.addParam('shiftLims', StringParam, label='Shift limits', default='1 1 1', help=" ")
+        form.addParam('shiftWay', IntParam, label='Shift limiting way', default=0, help=" ")
+        form.addParam('separation', IntParam, label='Separation in tomogram', default=0, help=" ")
+        form.addParam('threshold', FloatParam, label='Threshold', default=0.2, help=" ")
+        form.addParam('thresholdMode', IntParam, label='Threshold modus', default=0, help=" ")
+        form.addParam('threshold2', FloatParam, label='Second threshold', default=0.2, expertLevel=LEVEL_ADVANCED, help=" ")
+        form.addParam('thresholdMode2', IntParam, label='Second threshold modus', default=0, expertLevel=LEVEL_ADVANCED, help=" ")
+
+        form.addSection(label='Classification')
+        form.addParam('ccmatrix', BooleanParam, label='Compute  cross-correlation matrix', default=False, help=" ")
+        form.addParam('ccmatrixType', StringParam, label='Cross-correlation matrix type', default='bin 1; sym c1;', help=" ")
+        form.addParam('ccmatrixBatch', IntParam, label='Cross-correlation matrix batch', default=10, expertLevel=LEVEL_ADVANCED, help=" ")
 
     # --------------------------- INSERT steps functions --------------------------------------------
 
@@ -105,7 +120,6 @@ class DynamoSubTomoMRA(ProtTomoSubtomogramAveraging):
     # --------------------------- STEPS functions -------------------------------
 
     def convertInputStep(self):
-        # Put data in a folder
         inputVols = self.inputVolumes.get()
         fnDir = self._getExtraPath("data")
         makePath(fnDir)
@@ -114,12 +128,12 @@ class DynamoSubTomoMRA(ProtTomoSubtomogramAveraging):
         fhTable = open(self._getExtraPath("initial.tbl"), 'w')
         writeDynTable(fhTable, inputVols)
         fhTable.close()
-        # Generate "empty" table
+        # Generate "empty"/basic table
         # When it works, take table if exists as mltomo does with docfile
-
 
     def alignStep(self):
         pass
+        # pass as params to the commands all in the formulary + subtomo box size
 
     def createOutput(self):
         self.fhTable = open(self._getExtraPath("initial.tbl"), 'r')  # Change to "real.tbl"
@@ -127,7 +141,6 @@ class DynamoSubTomoMRA(ProtTomoSubtomogramAveraging):
         inputSet = self.inputVolumes.get()
         self.subtomoSet.copyInfo(inputSet)
         self.subtomoSet.copyItems(inputSet, updateItemCallback=self._updateItem)
-        # read dynamo table (convert)
         classesSubtomoSet = self._createSetOfClassesSubTomograms(self.subtomoSet)
         # classesSubtomoSet.classifyItems(updateClassCallback=self._updateClass)
         self.fhTable.close()
