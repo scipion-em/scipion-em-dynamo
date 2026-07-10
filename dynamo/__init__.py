d05b47fd3621b3284770d4deea784d467897714e
scipion <scipion@cnb.csic.es>
2026-07-10 13:37:04 +0200

    allowing i or icos for icosahedral symmetry

--- dynamo/__init__.py
+++ dynamo/__init__.py
@@ -32,7 +32,7 @@ import pyworkflow.utils as pwutils
 from pyworkflow import TOMO
 from .constants import *
 
-__version__ = '3.5.0'
+__version__ = '3.5.1'
 _logo = "icon.png"
 _references = ['CASTANODIEZ2012139']
 
